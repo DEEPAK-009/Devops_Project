c0fe7e05d2c212a993f5e9b1d8ec9dca1d1d13eb
deepak <152182723+DEEPAK-009@users.noreply.github.com>
2026-05-01 21:39:07 +0530

    x

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-const x = 10
+
 function App() {
   const [count, setCount] = useState(0)
 
